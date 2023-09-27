4ed1b6cdccd8b7a113c1a54a6cb43d3c8bff0e33
Shanmukha M K <shanmukhamk6@gmail.com>
2023-09-27 09:31:51 +0530

    initial commit
    initial commit

---
+++ src/main/java/com/sri/microservices/srispringcloudconfigservice/SriSpringCloudConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sri.microservices.srispringcloudconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SriSpringCloudConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SriSpringCloudConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sri/microservices/srispringcloudconfigservice/SriSpringCloudConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sri.microservices.srispringcloudconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SriSpringCloudConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
